6fd9475fdc5eed74b79b3633004af6eef81b1c5b
xtrapel <xd.rlc4m@slmail.me>
2025-05-25 13:18:07 +0100

    chore: fix issue with types

--- src/utils/create-component.ts
+++ src/utils/create-component.ts
@@ -1,7 +1,10 @@
-import { forwardRef } from 'react';
+import { forwardRef, ForwardRefRenderFunction } from 'react';
 
 type ComponentDefinition<Props> = (props: Props, ref?: any) => any;
 
 export function createComponent<Props>(definition: ComponentDefinition<Props>) {
-  return forwardRef(definition);
+  return forwardRef(definition) as unknown as ForwardRefRenderFunction<
+    any,
+    Props
+  >;
 }
